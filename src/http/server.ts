acaab3afd6033da80f2cc94c7b1f54ea03de9ffe
Igor Sobral <igorgmz1421@gmail.com>
2024-02-16 17:05:46 -0300

    feat: adding get cookie register

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,15 @@
 import fastify from "fastify";
+import cookie from '@fastify/cookie';
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
 
 const app = fastify();
+
+app.register(cookie, {
+  secret: "polls-app-nlw",
+  hook: 'onRequest', 
+})
 
 app.register(createPoll);
 app.register(getPoll);
