6f38a958578896397c62eb0203c97589f1093199
Igor Sobral <igorgmz1421@gmail.com>
2024-02-16 22:15:36 -0300

    .

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,22 +1,26 @@
 import fastify from "fastify";
-import cookie from '@fastify/cookie';
+import cookie from "@fastify/cookie";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
+import fastifyWebsocket from "@fastify/websocket";
+import { pollResults } from "./ws/poll-results";
 
 const app = fastify();
 
 app.register(cookie, {
   secret: "polls-app-nlw",
-  hook: 'onRequest', 
-})
+  hook: "onRequest",
+});
+
+app.register(fastifyWebsocket);
 
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
 
+app.register(pollResults);
+
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP Server running!");
 });
-
-
